f0b70b61496f033fbe4dab84ce781d0109b9293f
musawir007 <karlapolo143@gmail.com>
2024-08-04 01:24:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import smtplib as s
+try:
+    m = 1
+    while True:    
+        ob = s.SMTP("smtp.gmail.com",587)
+        ob.starttls()
+        ob.login("musawir2024@gmail.com","tdfy iwuh ulcn nznq")
+        subject = "musawir"
+        body=f"iam your frend {m}"
+        message = "subject:{}\n\n{}".format(subject,body)
 
-m = 1
-while True:    
-    ob = s.SMTP("smtp.gmail.com",587)
-    ob.starttls()
-    ob.login("musawir2024@gmail.com","tdfy iwuh ulcn nznq")
-    subject = "musawir"
-    body=f"iam your frend {m}"
-    message = "subject:{}\n\n{}".format(subject,body)
-
-    ob.sendmail("musawir2024@gmail.com","musawir8880@gmail.com",message)
-    ob.quit()
-    print("sending succesfull")
-    m +=1
+        ob.sendmail("musawir2024@gmail.com","musawir8880@gmail.com",message)
+        ob.quit()
+        print("sending succesfull")
+        m +=1
+except:
+    pass
